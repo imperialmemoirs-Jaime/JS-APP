206ae3304f3c9f9e3b6b25490b00233544527567
imperialmemoirs-Jaime <imperialmemoirs@gmail.com>
2025-08-11 12:21:28 +0000

    V1.2 of BlackJack Game

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let sum = firstCard + secondCard
+let sum = 0
+let allCards = []
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
-let allCards = [firstCard, secondCard]
+
 
 let messageEl = document.getElementById("message-el")
 let sumCount = document.getElementById("sum-count")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard(){
-   let randomNumber = Math.floor(Math.random() * 10) + 1
-   return randomNumber
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
+    isAlive = true
+    hasBlackjack = false
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    allCards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
@@ -26,34 +37,39 @@ function renderGame() {
     }
     sumCount.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message =  "You want to draw a new card?"    
+        message = "You want to draw a new card?"
     } else if (sum === 21) {
-        hasBlackjack =  true
+        hasBlackjack = true
         message = "You have got the Blackjack!!!!!!!"
     } else {
         isAlive = false
         message = "You are out of the game :("
     }
     messageEl.textContent = message
-   
 }
 
 function newCard() {
-    console.log("Drawing new card from the deck")
-    let card = getRandomCard()
-    allCards.push(card)
-    sum += card
-    renderGame()
+    if (isAlive === true && hasBlackjack === false) {
+        console.log("Drawing new card from the deck")
+        let card = getRandomCard()
+        allCards.push(card)
+        sum += card
+        renderGame()
+    }
 }
 
 function resetGame() {
     console.log("function triggered")
-    firstCard = 0
-    secondCard = 0
+
+    // Set all game state variables to their initial values
     sum = 0
-    allCards = [];
-    isAlive = true;
-    hasBlackjack = false;
-    message = "Want to play a round?";
-    renderGame();
+    allCards = []
+    isAlive = false
+    hasBlackjack = false
+    message = "Want to play a round?"
+
+    // Update the display elements directly
+    cardsEl.textContent = "Cards: "
+    sumCount.textContent = "Sum: " + sum
+    messageEl.textContent = message
 }
