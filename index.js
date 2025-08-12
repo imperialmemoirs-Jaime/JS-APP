95033031c998022da712ef7b766452f4f03149b7
imperialmemoirs-Jaime <imperialmemoirs@gmail.com>
2025-08-12 07:51:33 +0000

    Final Blackjack Game

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+let player = {
+    name: "Nolan",
+    chips: 23
+}
+ 
 let sum = 0
 let allCards = []
 let hasBlackjack = false
 let isAlive = false
 let message = ""
 
-
 let messageEl = document.getElementById("message-el")
 let sumCount = document.getElementById("sum-count")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + " : $" + player.chips
 
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random() * 13) + 1
